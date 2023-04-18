284f2e4af1448ae749e6fa95f82414d554d0ee10
csixteen <csixteen@protonmail.com>
2023-04-18 18:19:13 +0000

    Deploying to gh-pages from @ csixteen/honeybee@8df347ad3cf640207f338a28d9b6165bde54423c 🚀

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"honeybee":["",[["modules",[],["battery.rs","load_avg.rs","memory.rs","prelude.rs","time.rs"]],["output",[],["color.rs","dzen2.rs","i3bar.rs","lemonbar.rs","prelude.rs","term.rs","xmobar.rs"]]],["bar.rs","bridge.rs","config.rs","errors.rs","formatting.rs","lib.rs","modules.rs","output.rs","pango.rs","protocol.rs","timer.rs","types.rs","units.rs","utils.rs","widget.rs"]]\
+"honeybee":["",[["modules",[],["battery.rs","load_avg.rs","memory.rs","path_exists.rs","prelude.rs","time.rs"]],["output",[],["color.rs","dzen2.rs","i3bar.rs","lemonbar.rs","prelude.rs","term.rs","xmobar.rs"]]],["bar.rs","bridge.rs","config.rs","errors.rs","formatting.rs","lib.rs","modules.rs","output.rs","pango.rs","protocol.rs","timer.rs","types.rs","units.rs","utils.rs","widget.rs"]]\
 }');
 createSourceSidebar();
