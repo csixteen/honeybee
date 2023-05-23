afdc7b31e143fadd91c2f851f589ce14fc63c6b6
csixteen <csixteen@protonmail.com>
2023-05-23 18:59:29 +0000

    Deploying to gh-pages from @ csixteen/honeybee@d7e7a112f7977e8e27a853bd0fbef7f07e748c51 🚀

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"honeybee":["",[["modules",[],["battery.rs","disk.rs","file_contents.rs","load_avg.rs","memory.rs","path_exists.rs","prelude.rs","run_watch.rs","time.rs","wireless.rs"]],["output",[],["color.rs","dzen2.rs","i3bar.rs","lemonbar.rs","prelude.rs","term.rs","xmobar.rs"]]],["bar.rs","bridge.rs","config.rs","errors.rs","formatting.rs","lib.rs","modules.rs","output.rs","pango.rs","protocol.rs","timer.rs","types.rs","units.rs","utils.rs","widget.rs"]]\
+"honeybee":["",[["modules",[],["battery.rs","disk.rs","file_contents.rs","load_avg.rs","memory.rs","path_exists.rs","prelude.rs","run_watch.rs","time.rs","wireless.rs"]],["output",[],["color.rs","dzen2.rs","i3bar.rs","lemonbar.rs","prelude.rs","term.rs","xmobar.rs"]]],["bar.rs","bridge.rs","config.rs","errors.rs","formatting.rs","lib.rs","modules.rs","net_iface.rs","output.rs","pango.rs","protocol.rs","timer.rs","types.rs","units.rs","utils.rs","widget.rs"]]\
 }');
 createSourceSidebar();
